f3d6c80daf3c79758807113818e8fee9bb23db1d
Ilya Eranov <ilya.erannov123454321@gmail.com>
2024-09-17 00:58:14 +0500

    creating main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,16 @@
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { setupStore } from './store/store'
+import App from './app/App'
+
+const store = setupStore()
 
 createRoot(document.getElementById('root')!).render(
-  <></>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </BrowserRouter>
 )
 
