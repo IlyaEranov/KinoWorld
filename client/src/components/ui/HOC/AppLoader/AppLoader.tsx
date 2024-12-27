0266d1baeb34506a478b8d910430d07597c1ff41
Ilya Eranov <ilya.erannov123454321@gmail.com>
2024-12-27 20:27:32 +0500

    fix: AppLoader

--- client/src/components/ui/HOC/AppLoader/AppLoader.tsx
+++ client/src/components/ui/HOC/AppLoader/AppLoader.tsx
@@ -1,10 +1,6 @@
 import { FC, ReactNode, useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "../../../../hooks/redux"
-<<<<<<< HEAD
 import {getMovie, getKinoTop10, getSeries, searchKino } from "../../../../store/reducers/KinoSlice"
-=======
-import {getMovie, getKinoTop10, getSeries } from "../../../../store/reducers/KinoSlice"
->>>>>>> 1b120a2f59e059d3e237821a852199ae103a5f53
 import Spinner from "../Spinner/Spinner"
 import ErrorPage from "../ErrorPage/ErrorPage"
 
@@ -20,10 +16,7 @@ const AppLoader: FC<AppLoaderProps> = ({children}) => {
         dispatch(getKinoTop10({limit: 5}))
         dispatch(getMovie({limit: 6, notNullFields: ["poster.url", "description", "top250"]}))
         dispatch(getSeries({limit: 6, notNullFields: ["poster.url", "description", "top250"]}))
-<<<<<<< HEAD
         dispatch(searchKino(""))
-=======
->>>>>>> 1b120a2f59e059d3e237821a852199ae103a5f53
     }, [])
 
     const {isLoading, error} = useAppSelector(state => state.KinoReducer)
