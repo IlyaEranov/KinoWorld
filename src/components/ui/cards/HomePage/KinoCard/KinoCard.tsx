b28b3778394e51c51be530035904ab589053039b
Ilya Eranov <ilya.erannov123454321@gmail.com>
2025-02-23 16:24:32 +0500

    feat: adding links

--- src/components/ui/cards/HomePage/KinoCard/KinoCard.tsx
+++ src/components/ui/cards/HomePage/KinoCard/KinoCard.tsx
@@ -1,21 +1,25 @@
 import { FC } from "react"
 import s from "./KinoCard.module.scss"
+import { Link } from "react-router-dom"
+import { kinoUtil } from "../../../../../utils/kinoUtil"
 
 interface KinoCardProps{
+    id: string
     image: string
     name: string
     description: string
+    type: string
 }
 
-const KinoCard: FC<KinoCardProps> = ({image, name, description}) => {
+const KinoCard: FC<KinoCardProps> = ({image, name, description, type, id}) => {
     return(
-        <div className={s.card}>
+        <Link to={`/watch/${kinoUtil.linkType(type)}/${id}`} className={s.card}>
             <img className={s.image} src={image}/>
             <div className={s.description}>
                 <h2 className={s.h2}>{name}</h2>
                 <p className={s.p}>{description}</p>
             </div>
-        </div>
+        </Link>
     )
 }
 
--- src/components/ui/cards/SearchSuggestionCard/SearchSuggestionCard.tsx
+++ src/components/ui/cards/SearchSuggestionCard/SearchSuggestionCard.tsx
@@ -1,18 +1,20 @@
 import { FC } from "react"
 import s from "./SearchSuggestionCard.module.scss"
 import { kinoUtil } from "../../../../utils/kinoUtil"
+import { Link } from "react-router-dom"
 
 interface SearchSuggestionCardProps{
+    id: string
     image: string
     name: string
     rating: number
-    type?: string
+    type: string
     year: number
 }
 
-const SearchSuggestionCard: FC<SearchSuggestionCardProps> = ({image, name, rating, type, year}) => {
+const SearchSuggestionCard: FC<SearchSuggestionCardProps> = ({id, image, name, rating, type, year}) => {
     return(
-        <div className={s.card}>
+        <Link to={`/watch/${kinoUtil.linkType(type)}/${id}`} className={s.card}>
             <img className={s.image} src={image}/>
             <div className={s.description}>
                 <h4>{name}</h4>
@@ -20,11 +22,11 @@ const SearchSuggestionCard: FC<SearchSuggestionCardProps> = ({image, name, ratin
                     <span className={s.rating} style={{
                         color: kinoUtil.ratingColor(rating)
                     }}>{rating}</span>
-                    {type && <p>{kinoUtil.typeKino(type)},</p>}
+                    <p>{kinoUtil.typeKino(type)},</p>
                     <p>{year}</p>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
